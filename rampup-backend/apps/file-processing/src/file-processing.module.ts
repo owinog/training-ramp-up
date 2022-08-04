21bf14ea8a3a51ebe57555752d05cdf5f662be5b
owinog <owingunawardena0123@gmail.com>
2022-08-04 22:24:01 +0530

    set up bull queue

--- rampup-backend/apps/file-processing/src/file-processing.module.ts
+++ rampup-backend/apps/file-processing/src/file-processing.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FileProcessingController } from './file-processing.controller';
 import { FileProcessingService } from './file-processing.service';
+import { BullModule } from '@nestjs/bull';
+import { StudentListModule } from './student-list/student-list.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    StudentListModule,
+  ],
   controllers: [FileProcessingController],
   providers: [FileProcessingService],
 })
---
+++ rampup-backend/apps/file-processing/src/student-list/student-list.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+
+@Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'studentList',
+    }),
+  ],
+})
+export class StudentListModule {}
