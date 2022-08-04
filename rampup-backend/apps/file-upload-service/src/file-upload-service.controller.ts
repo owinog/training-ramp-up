acf72ba07cc1750d0d43d5b2c94cd694c820e3e0
owinog <owingunawardena0123@gmail.com>
2022-08-04 10:18:17 +0530

    file uploads backend endpoint

--- rampup-backend/apps/file-upload-service/src/file-upload-service.controller.ts
+++ rampup-backend/apps/file-upload-service/src/file-upload-service.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { FileUploadServiceService } from './file-upload-service.service';
+import { Express } from 'express';
 
 @Controller()
 export class FileUploadServiceController {
-  constructor(private readonly fileUploadServiceService: FileUploadServiceService) {}
+  constructor(
+    private readonly fileUploadServiceService: FileUploadServiceService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.fileUploadServiceService.getHello();
+  }
+
+  @UseInterceptors(
+    FileInterceptor('file', { dest: './apps/file-upload-service/src/uploads' }),
+  )
+  @Post('file')
+  uploadFile(
+    @Body() body: string,
+    @UploadedFile()
+    file: Express.Multer.File,
+  ) {
+    return { body, file: file.buffer.toString() };
   }
 }
